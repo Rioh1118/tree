7e744098418f241e129d3348bb63db9ea6210d9a
Rioh <90512887+Rioh1118@users.noreply.github.com>
2024-12-23 12:57:19 +0900

    BinaryTreeクラスのNode定義を別ファイルに

--- src/com/github/rioh1118/binary/BinaryTree.java
+++ src/com/github/rioh1118/binary/BinaryTree.java
@@ -3,57 +3,9 @@ package com.github.rioh1118.binary;
 import com.github.rioh1118.Tree;
 import java.util.Optional;
 
-class Node<K, V> {
-  private K key;
-  private V value;
-
-  private Node<K, V> left;
-  private Node<K, V> right;
-
-  // コンストラクタ
-  public Node(K key, V value) {
-    this.key = key;
-    this.value = value;
-    this.left = null;
-    this.right = null;
-  }
-
-  // ゲッタ、 セッタ
-  public K getKey() {
-    return key;
-  }
-
-  public void setKey(K key) {
-    this.key = key;
-  }
-
-  public V getValue() {
-    return value;
-  }
-
-  public void setValue(V value) {
-    this.value = value;
-  }
-
-  public Node<K, V> getLeft() {
-    return left;
-  }
-
-  public void setLeft(Node<K, V> left) {
-    this.left = left;
-  }
-
-  public Node<K, V> getRight() {
-    return right;
-  }
-
-  public void setRight(Node<K, V> right) {
-    this.right = right;
-  }
-}
-
-
 public class BinaryTree<K, V> implements Tree<K, V> {
+  private Node<K, V> root;
+
   @Override
   public boolean insert(V value) {
     // :TODO ここに挿入処理を実装してください
---
+++ src/com/github/rioh1118/binary/Node.java
@@ -0,0 +1,50 @@
+package com.github.rioh1118.binary;
+
+class Node<K, V> {
+  private K key;
+  private V value;
+
+  private Node<K, V> left;
+  private Node<K, V> right;
+
+  // コンストラクタ
+  public Node(K key, V value) {
+    this.key = key;
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+
+  // ゲッタ、 セッタ
+  public K getKey() {
+    return key;
+  }
+
+  public void setKey(K key) {
+    this.key = key;
+  }
+
+  public V getValue() {
+    return value;
+  }
+
+  public void setValue(V value) {
+    this.value = value;
+  }
+
+  public Node<K, V> getLeft() {
+    return left;
+  }
+
+  public void setLeft(Node<K, V> left) {
+    this.left = left;
+  }
+
+  public Node<K, V> getRight() {
+    return right;
+  }
+
+  public void setRight(Node<K, V> right) {
+    this.right = right;
+  }
+}
